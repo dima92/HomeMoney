3261bd9a3b1f2c3989f5451b5aaa21d9f8993832
Andrei-PC <Andrei-PC@DESKTOP-NMVCPT8>
2018-07-25 22:36:28 +0300

    регистрация страницы логин

--- Web/ClientApp/src/app/auth/login/login.component.ts
+++ Web/ClientApp/src/app/auth/login/login.component.ts
@@ -1,7 +1,7 @@
-﻿import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-login',
+    selector: 'login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.css'],
 })
