fdee49fd57855760557dffdbe3c15d4c409bd98e
Andrei-PC <Andrei-PC@DESKTOP-NMVCPT8>
2018-08-03 18:48:41 +0300

    фиксинг багов

--- Web/ClientApp/src/app/auth/registration/registration.component.ts
+++ Web/ClientApp/src/app/auth/registration/registration.component.ts
@@ -1,12 +1,42 @@
-﻿import { Component, OnInit } from '@angular/core';
+
+
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { User } from "../../models/user.model";
+
 
 @Component({
-    selector: 'app-registration',
-    templateUrl: './registration.component.html',
-    styleUrls: ['./registration.component.css'],
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-    ngOnInit(): void {
-        throw new Error("Method not implemented.");
-    }
+
+  form: FormGroup;
+
+  constructor(
+    private router: Router,
+    private title: Title
+  ) {
+    title.setTitle('Регистрация');
+  }
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      //'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails.bind(this)),
+      'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      'name': new FormControl(null, [Validators.required]),
+      'agree': new FormControl(false, [Validators.requiredTrue])
+    });
+  }
+
+  onSubmit() {
+    debugger;
+    const { email, password, name } = this.form.value;
+    const user = new User(email, password, name);
+
+  }
+
 }
