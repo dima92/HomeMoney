852bfd8a3c4a596d87a05c122282c6712fb709cb
Andrei-PC <Andrei-PC@DESKTOP-NMVCPT8>
2018-10-09 22:59:47 +0300

    рефакторинт

--- WebAppAngular/ClientApp/src/app/models/user.model.ts
+++ WebAppAngular/ClientApp/src/app/models/user.model.ts
@@ -1,6 +1,8 @@
 export class User {
-    public email: string;
-    public password: string;
-    public name: string;
-    public id?: number;
+  constructor(
+    public email: string,
+    public password: string,
+    public name: string,
+    public id?: number
+  ) { }
 }
